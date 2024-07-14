1d75bbbb6c56ba915785b2461f0322767c233e7f
HarshVasoya <vasoyaharsh504@gmail.com>
2024-07-14 19:15:27 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: Quote_Home(),
       initialRoute: '/',
       routes: {
         'HomePage': (context) => const QuoteHome(),
